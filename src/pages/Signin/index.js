f02e65403a826fcef6c88885e1046c35a945baf9
alejandro <alejandrober96@gmail.com>
2023-10-26 16:12:28 -0500

    Add onsubmit dummy function to signin

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -3,11 +3,19 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function Signin() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submit')
+    const formData = new FormData(e.target);
+    console.log(formData.get('email'));
+    console.log(formData.get('password'));
+  }
   return (
-    <Form style={{padding: '5rem 0'}}>
+    <Form style={{padding: '5rem 0'}} onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" name="email" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -15,7 +23,7 @@ function Signin() {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" name="password"/>
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
