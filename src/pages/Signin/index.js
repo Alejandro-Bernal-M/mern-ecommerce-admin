ae29ae5a3c372ec0d404c123dd8b61a7cc65f87b
alejandro <alejandrober96@gmail.com>
2023-10-26 15:45:11 -0500

    Add signin form

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function Signin() {
   return (
-    <div>Signin
-    
-    </div>
+    <Form style={{padding: '5rem'}}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   )
 }
 
