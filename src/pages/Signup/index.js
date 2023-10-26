ba2b138dc66ded3e38333c50b8c318ebdfdd9c03
alejandro <alejandrober96@gmail.com>
2023-10-26 16:02:01 -0500

    Add signup form

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Col, Row } from 'react-bootstrap';
 
 function Signup() {
   return (
-    <div>Signup</div>
+    <Form style={{padding: '5rem 0 '}}>
+      <Row >
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Firs Name</Form.Label>
+            <Form.Control type="text" placeholder="First name" />
+          </Form.Group>
+      </Col>
+      <Col md={6}>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control type="text" placeholder="Last name" />
+        </Form.Group>
+        </Col>
+       </Row>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   )
 }
 
