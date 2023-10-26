626458c1999891d4d13d5a9775d11590d735c197
alejandro <alejandrober96@gmail.com>
2023-10-26 15:12:39 -0500

    Add layout component to app

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
 
+import './App.css';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <h1>Hello</h1>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
